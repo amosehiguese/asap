95ac7d0d695f74b28fcf09c18d32e08f0703679d
amosehiguese <amosehiguese@gmail.com>
2024-07-02 15:51:09 +0100

    feat: implement forpassword page

--- modules/web/src/pages/ForgotPassword.tsx
+++ modules/web/src/pages/ForgotPassword.tsx
@@ -1,11 +1,41 @@
 import { Helmet } from "react-helmet-async";
+import {
+  RedirectLink,
+  ForgotPasswordForm
+} from "../components/auth";
 
 
 const ForgotPassword = () => {
   return (
-    <div>
+    <>
+      {/* Header */}
+      <Helmet>
+        <title> Forgot Password | Syp</title>
+        <meta name="description" content="Reset your Syp password quickly and easily. Receive a reset link if your email is registered with us." />
+        <link rel="canonical" href={`${window.location.origin}/auth/forgot-password`} />
+      </Helmet>
 
-    </div>
+      {/* Main */}
+      <section className="flex flex-col gap-y-6">
+        <div className="flex flex-col gap-y-3">
+          <h3 className="text-4xl text-fluid-h3 font-bold">Let us help you</h3>
+          <p className="text-lg text-fluid-p">You'll receive a password reset link if your email is registered with us</p>
+        </div>
+
+        <div>
+          <ForgotPasswordForm/>
+        </div>
+
+        <RedirectLink
+          pageName="Login"
+          text="Go back?"
+          to="auth/login"
+        />
+
+      </section>
+
+
+    </>
   )
 }
 
