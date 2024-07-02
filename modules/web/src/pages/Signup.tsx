a3fec338f91a70c1a25953b2a3410045a917ec48
amosehiguese <amosehiguese@gmail.com>
2024-07-02 16:16:22 +0100

    feat: implement signup page

--- modules/web/src/pages/Signup.tsx
+++ modules/web/src/pages/Signup.tsx
@@ -1,9 +1,22 @@
+import { Helmet } from "react-helmet-async"
+import { SignupForm } from "../components/auth"
 
 const Signup = () => {
   return (
-    <div>
-      Signup
-    </div>
+
+    <>
+      <Helmet>
+        <title>Signup | Syp</title>
+        <meta name="description" content="Signup to Syp, your AI-based symptom checker." />
+        <link rel="canonical" href={`${window.location.origin}/auth/signup`} />
+      </Helmet>
+
+      <div className="flex flex-col gap-y-8">
+        <div className="text-4xl font-bold text-fluid-h4">Signup</div>
+      </div>
+
+      <SignupForm/>
+    </>
   )
 }
 
